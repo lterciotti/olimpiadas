b3a7e47e66c8d42638489191e5a62e56bfc34b46
lterciotti <107123915+lterciotti@users.noreply.github.com>
2024-09-06 12:24:42 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+        {
+          titulo: "Rayssa Leal",
+          descricao: "Rayssa Leal é uma skatista brasileira que se tornou a mais jovem medalhista olímpica da história do Brasil. Com seu estilo único e carisma, ela conquistou o mundo do skate e inspirou milhares de jovens.",
+          link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+          tags: "skate fada sk8"
+        },
+        {
+          titulo: "Beatriz Souza",
+          descricao: "Beatriz Souza é uma judoca brasileira, campeã olímpica nos Jogos Olímpicos de Paris 2024. Sua técnica apurada e determinação a consagraram como uma das maiores judocas da atualidade.",
+          link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+          tags: "judô judo judoca judoka"
+        },
+        {
+          titulo: "Rebeca Andrade",
+          descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou diversas medalhas em competições internacionais. Sua versatilidade e força a tornaram uma das principais atletas da ginástica artística mundial.",
+          link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+          tags: "ginástica ginastica ouro"
+        }
+];
+
